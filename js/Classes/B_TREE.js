6a58df9f43ee52beda01b1ffe158084e08743a24
Armen <azakaryan@macadamian.com>
2015-05-21 23:22:18 +0400

    UI has been added for delete and insert

--- js/Classes/B_TREE.js
+++ js/Classes/B_TREE.js
@@ -1,32 +1,34 @@
 'use strict';
 
-function searchHelper(node, key){
-    if( node != null ) {
-        for( var i = 0; i < node.size; i++ ) {
+function searchHelper(node, key) {
+    if (node != null) {
+        for (var i = 0; i < node.size; i++) {
             var curentKey = node.keys[i];
             // TODO this if need to me removed
-            if(curentKey != null){
-                if( curentKey == key ){
+            if (curentKey != null) {
+                if (curentKey == key) {
                     return true;
-                } else if( curentKey > key ){
+                } else if (curentKey > key) {
                     // Case when need to go to next level
                     // Check if the Node is leaf then return false
-                    if( node.leaf ){
+                    if (node.leaf) {
                         return false;
-                    } else return searchHelper(node.links[i], key);
+                    } else
+                        return searchHelper(node.links[i], key);
                 }
             }
         }
         // Recurse for the last link
         return searchHelper(node.links[node.size], key);
-    }
+    } 
     // if we got here then
-    else return false;
+    else
+        return false;
 }
 
 
 define(['Node'], function(Node) {
-
+    
     function B_TREE(t) {
         var numberOfNodes = 1, numberOfElements, height = 0, self = this;
         // Static method set t (maxDegree)   
@@ -34,10 +36,10 @@ define(['Node'], function(Node) {
         
         
         this.root = new Node();
-        
+
         /*SEARCH*/
-        this.search = function(key){
-            if( !searchHelper(self.root, key) ){
+        this.search = function(key) {
+            if (!searchHelper(self.root, key)) {
                 alert("Element Does Not Exist");
                 return false;
             } else {
@@ -50,64 +52,64 @@ define(['Node'], function(Node) {
 
 
         /*INSERT*/
-        function shiftRightByOne(array, size, startIndexToShift){
-            for (var i = size; i > startIndexToShift ; i--){
-                array[i] = array[i-1];
+        function shiftRightByOne(array, size, startIndexToShift) {
+            for (var i = size; i > startIndexToShift; i--) {
+                array[i] = array[i - 1];
             }
         }
-        function getIndexToInsert(node, key ){
+        function getIndexToInsert(node, key) {
             var i = 0;
-            while ( i < node.size){
-                if ( node.keys[i] > key ) {
+            while (i < node.size) {
+                if (node.keys[i] > key) {
                     return i;
                 }
                 i++;
             }
-            return  i;
+            return i;
         }
-        function goToNextLevel(node, key){
+        function goToNextLevel(node, key) {
             var flag = false;
-            for( var i = 0; i < node.size; i++ ) {
-                if( node.keys[i] > key && !flag ) {
+            for (var i = 0; i < node.size; i++) {
+                if (node.keys[i] > key && !flag) {
                     flag = true;
                     insertHelper(node, node.links[i], key);
                 }
             }
             // Case when the next level direction goes from last link of the Node
-            if( !flag ){
+            if (!flag) {
                 insertHelper(node, node.links[node.size], key);
             }
         }
-        function split(parent, node){
+        function split(parent, node) {
             var size = node.size;
             var i = 0;
             var newNode = new Node();
             numberOfNodes++;
-            if (!node.leaf){
+            if (!node.leaf) {
                 newNode.leaf = false;
             }
-            var midleKey = node.keys[t-1];
-            node.keys[t-1] = null;
+            var midleKey = node.keys[t - 1];
+            node.keys[t - 1] = null;
             node.size--;
             // Copy last half of the elements and appropriate links into the newly created node
-            for( ; i < size; i++ ) {
-                if( i > t - 1 ) {
+            for (; i < size; i++) {
+                if (i > t - 1) {
                     // Move to new array and remove old key
-                    newNode.keys[i-t] = node.keys[i];
+                    newNode.keys[i - t] = node.keys[i];
                     node.keys[i] = null;
                     // Move to new array and remove old link
-                    newNode.links[i-t] = node.links[i];
+                    newNode.links[i - t] = node.links[i];
                     node.links[i] = null;
                     node.size--;
                     newNode.size++;
                 }
             }
             // Copy the last link
-            newNode.links[i-t] = node.links[i];
+            newNode.links[i - t] = node.links[i];
             node.links[i] = null;
 
             // Case when node is a self.root
-            if ( parent == null ) {
+            if (parent == null) {
                 self.root = new Node();
                 numberOfNodes++;
                 self.height++;
@@ -123,17 +125,17 @@ define(['Node'], function(Node) {
                 shiftRightByOne(parent.keys, parent.size, index);
                 parent.keys[index] = midleKey;
                 // For Links
-                shiftRightByOne(parent.links, parent.size+1, index+1);
-                parent.links[index+1] = newNode;
+                shiftRightByOne(parent.links, parent.size + 1, index + 1);
+                parent.links[index + 1] = newNode;
                 parent.size++;
                 return parent;
             }
         }
         function insertHelper(parent, node, key) {
-            if( node != null){
-                if( node.size < 2*t-1 ){
+            if (node != null) {
+                if (node.size < 2 * t - 1) {
                     // Case when there is a redundancy (not full)
-                    if(node.leaf){
+                    if (node.leaf) {
                         // Insert inside the leaf Node
                         var index = getIndexToInsert(node, key);
                         shiftRightByOne(node.keys, node.size, index);
@@ -147,13 +149,13 @@ define(['Node'], function(Node) {
                     // When current node is full
                     node = split(parent, node);
                     goToNextLevel(node, key);
-                    //insertHelper(parent,node, key);
+                //insertHelper(parent,node, key);
                 }
             }
         }
-
-        this.insert = function(key){
-            if( !searchHelper(self.root, key) ){
+        
+        this.insert = function(key) {
+            if (!searchHelper(self.root, key)) {
                 insertHelper(null, self.root, key);
                 numberOfElements++;
             } else {
@@ -163,11 +165,254 @@ define(['Node'], function(Node) {
         /*END INSERT*/
 
 
-        
 
 
 
+
+ /*DELETE*/
+
+    function shiftLeftByOne(array, size, startIndexToShift){
+        for (var i = startIndexToShift; i < size-1 ; i++ ){
+            array[i] = array[i+1];
+        }
+        array[size-1] = null;
+    }
+    function getNodeFromLeftSibling(parent, index){
+        var node = parent.links[index];
+        var sibling = parent.links[index - 1];
+
+        shiftRightByOne(node.keys, node.size, 0);
+        shiftRightByOne(node.links, node.size+1, 0);
+        node.size++;
+        node.keys[0] = parent.keys[index - 1];
+        node.links[0] = sibling.links[sibling.size];
+
+        parent.keys[index - 1] = sibling.keys[sibling.size - 1];
+
+        sibling.keys[sibling.size - 1] = null;
+        sibling.links[sibling.size] = null;
+        sibling.size--;
+    }
+    function getNodeFromRightSibling(parent, index){
+        var node = parent.links[index];
+        var sibling = parent.links[index + 1];
+
+        node.keys[node.size] = parent.keys[index];
+        node.links[node.size + 1] = sibling.links[0];
+        node.size++;
+        parent.keys[index] = sibling.keys[0];
+        // left shift the sibling by one
+        shiftLeftByOne(sibling.keys, sibling.size, 0);
+        shiftLeftByOne(sibling.links, sibling.size+1, 0);
+        sibling.size--;
+    }
+    function join(parent, parentIndex){
+        var left =  parent.links[parentIndex];
+        var right = parent.links[parentIndex + 1];
+        var parentKey = parent.keys[parentIndex];
+
+        // Parent Modifications
+        shiftLeftByOne(parent.keys, parent.size, parentIndex);
+        shiftLeftByOne(parent.links, parent.size + 1, parentIndex+1);
+        parent.size--;
+
+        // Children modifications. Both child has the same length t-1
+        left.keys[t-1] = parentKey;
+        for ( var i = t; i < 2*t-1; i++ ){
+            left.keys[i] = right.keys[i-t];
+            left.links[i] = right.links[i-t];
+        }
+        left.links[2*t-1] = right.links[t-1];
+        left.size = 2*t-1;
+        return left;
+    }
+    function removeFromLeaf(node, key){
+        //TODO
+        for(var i = 0; i < node.size; i++){
+            if(node.keys[i] == key){
+                var predKey = node.keys[i];
+                shiftLeftByOne(node.keys, node.size, i);
+                node.size--;
+                return predKey;
+            }
+        }
+        return 0;
+    }
+    function getPredecessor(node){
+        if (node.leaf){
+            var predKey = node.keys[node.size - 1];
+            node.keys[node.size - 1] = null;
+            node.size--;
+            return predKey;
+        } else {
+            var lastChildNode = node.links[node.size];
+            if(lastChildNode.size >= t ){
+                return getPredecessor(lastChildNode);
+            } else if(node.links[node.size - 1].size >= t) {
+                // When not enougth elements in the last child And more elements in prev of the last child
+                getNodeFromLeftSibling(node, node.size);
+                return getPredecessor(lastChildNode);
+            } else {
+                // join // When not enougth elements both children
+                node = join(node, node.size -1);
+                return getPredecessor(node);
+            }
+        }
+    }
+    function getSuccessor(node){
+        if (node.leaf){
+            var predKey = node.keys[0];
+            shiftLeftByOne(node.keys, node.size, 0);
+            node.size--;
+            return predKey;
+        } else {
+            var firstChildNode = node.links[0];
+            if(firstChildNode.size >= t ){
+                return getPredecessor(firstChildNode);
+            } else if(node.links[0].size >= t){
+                // When not enougth elements in the first child, but enough in it's sibling
+                getNodeFromRightSibling(node, 0);
+                return getPredecessor(firstChildNode);
+            } else {
+                // join // When not enougth elements both children
+                node = join(node, 0);
+                return getPredecessor(node);
+            }
+        }
+    }
+    function deleteHelper(node, key) {
+        // If leaf delete. If we got here, then we are sure that the size of the node either >=t or the node is the root
+        if (node.leaf) {
+            removeFromLeaf(node, key);
+            return true;
+        } else {
+            for (var i = 0; i < node.size; i++) {
+                if (node.keys[i] >= key) {
+                    // Founded
+                    if (node.links[i].size >= t) {
+                        if (node.keys[i] == key) {
+                            node.keys[i] = getPredecessor(node.links[i]);
+                        } else {
+                            deleteHelper(node.links[i], key);
+                        }
+                    } else {
+                        // Need join or get node from either sibling
+                        if (node.links[i + 1].size >= t) {
+                            if (node.keys[i] == key) {
+                                node.keys[i] = getSuccessor(node.links[i + 1]);
+                            } else {
+                                // get node from right sibling
+                                getNodeFromRightSibling(node, i);
+                                deleteHelper(node.links[i], key);
+                            }
+                        } else if (i > 0 && node.links[i - 1].size >= t) {
+                            // get node from left sibling
+                            getNodeFromLeftSibling(node, i);
+                            if (node.keys[i] == key) {
+                                node.keys[i] = getPredecessor(node.links[i]);
+                            } else {
+                                deleteHelper(node.links[i], key);
+                            }
+                        } else {
+                            // Join // here, the value goes to the next row thus we call deleteHelper()
+                            node = join(node, i);
+                            deleteHelper(node, key);
+                        }
+                    }
+                    return true;
+                }
+                // Continue Loop
+            }
+            // if code gets here, it means that the if condition in the for loop has not been true for all elements
+            if (node.links[node.size].size >= t) {
+                deleteHelper(node.links[node.size], key);
+            } else {
+                // Need join or get node from either sibling
+                if (node.links[node.size - 1] && node.links[node.size - 1].size >= t) {
+                    getNodeFromLeftSibling(node, node.size);
+                    deleteHelper(node.links[node.size], key);
+                } else {
+                    // join
+                    node = join(node, node.size - 1);
+                    deleteHelper(node, key);
+                }
+            }
+            return true;
+        }
     }
 
+
+    this.delete = function(key){
+        if( searchHelper(self.root, key) ){
+            deleteHelper(self.root, key);
+        } else {
+            alert("Sorry, Element with such key does not exist");
+        }
+    }
+    /*END DELETE*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        /*format data for UI*/
+        this.format = function(b_tree) {
+            var maxHeight = {count:0};
+            function recurse(prev) {
+                var nextLevelNodes = [];
+                
+                for (var i = 0; i < prev.nodes.length; i++) {
+                    var curentNode = prev.nodes[i];
+                    if(curentNode){
+                        for (var j = 0; j <= curentNode.size; j++) {
+                            nextLevelNodes.push(curentNode.links[j]);
+                        }
+                    }
+                    nextLevelNodes.push(null);
+                }
+                
+                maxHeight.count = prev.level + 1;
+                prev.next = {
+                    level: maxHeight.count,
+                    maxHeight: maxHeight,
+                    nodes: nextLevelNodes,
+                    next: null
+                };
+
+                // if get to the leaf then stop otherwise call the recursion again    
+                if (nextLevelNodes[0] && !nextLevelNodes[0].leaf) {
+                    recurse(prev.next);
+                }
+            
+            }
+            var formatedData = {
+                level: 0,
+                maxHeight: maxHeight,
+                nodes: [b_tree.root],
+                next: null
+            }
+
+            recurse(formatedData);
+
+            formatedData.maxHeight = maxHeight;
+            return formatedData; 
+        
+        }
+        /*END format data for UI*/
+
+    }
+    
     return B_TREE;
 });
--- js/vm/B_Tree.js
+++ js/vm/B_Tree.js
@@ -2,39 +2,67 @@
 
 define(['jquery', 'ko', 'B_TREE', 'bootstrap'], function($, ko, B_TREE) {
     function B_Tree() {
+        var self = this;
+        var b_tree;
+        
         this.insertValue = ko.observable();
         
-        var b_tree = new B_TREE(2);
-        //new EventsKoModel(b_tree);
+        //var b_tree = new B_TREE(2);
         
-        
+        this.istreeExist = ko.observable(false);
+        this.degree = ko.observable();
+        this.tree = ko.observable();
+
+
+        this.makeTree = function(){
+            var value = self.degree();
+            if(!isNaN(value)){
+                b_tree = new B_TREE(value);
+                self.istreeExist(true);
+            } else {
+                alert("Please insert a number");
+            }
+        }
+
+/*        
         b_tree.insert(3);
         b_tree.insert(7);
         b_tree.insert(9);
         
         b_tree.insert(23);
         
+        b_tree.insert(45);
+        b_tree.insert(1);
+        b_tree.insert(5);
+        b_tree.insert(14);
+        b_tree.insert(25);
+        b_tree.insert(24);
+        b_tree.insert(13);
+        b_tree.insert(11);
+        b_tree.insert(8);
+        b_tree.insert(19);
+        b_tree.insert(4);
+        b_tree.insert(31);
+        b_tree.insert(35);
+        b_tree.insert(56);
+        b_tree.insert(8);
+        b_tree.insert(10);
+
+        b_tree.delete(56);
+        b_tree.delete(31);
+*/
+
+
+        //this.tree = ko.observable(b_tree.format(b_tree));
         
         
-        
+        /*
         this.degrees = [
             {id: 1,val: 3}, 
             {id: 2,val: 4}, 
             {id: 3,val: 5}
-        ];
-        
-        
-        
-        function EventsKoModel(data) {
-            var self = this;
-            self.event = ko.observable();
-            self.event.subscribe(function() {
-                debugger
-                console.log("something changed");
-            });
-            komapping.fromJS(data, {}, self);
-        }
-        
+        ];*/
+
         
         
         
@@ -45,8 +73,29 @@ define(['jquery', 'ko', 'B_TREE', 'bootstrap'], function($, ko, B_TREE) {
             return true;
         }
         
-        this.add = function() {
-            debugger
+        this.insertedValue = ko.observable();
+        this.deletedValue = ko.observable();
+
+        this.insert = function() {
+            var value = self.insertedValue();
+            if(!isNaN(value)){
+                b_tree.insert(value);
+                self.tree(b_tree.format(b_tree));
+                self.insertedValue('');   
+            } else {
+                alert("Please insert a number");
+            }
+        }
+
+        
+        this.remove = function(dd) {
+            var value = self.deletedValue();
+            if(!isNaN(value)){
+                b_tree.delete(value);
+                self.tree(b_tree.format(b_tree));   
+            } else {
+                alert("Please insert a number");
+            }
         }
     }
     
